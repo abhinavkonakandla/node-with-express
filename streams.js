0c842b4567a41e65897b9997af7731895a4b66d2
abhinavkonakandla <abhinavkonakandla@gmail.com>
2024-01-19 18:24:47 +0530

    intro to strings

--- streams.js
+++ streams.js
@@ -1,2 +1,17 @@
 //streams is also one of the most useful topic in node js
 // this is used for tochange the text from one file to another file.
+//it is also collection of data like array and strings.
+//The difference is that it not stores all at once.
+//example:
+/*
+if we want to store a file streams helps us to store in 
+small chunks or parts of data.
+
+streams works on a concept called buffer.
+
+buffer ---> a buffer is a temporary memory. 
+*/
+
+
+
+
